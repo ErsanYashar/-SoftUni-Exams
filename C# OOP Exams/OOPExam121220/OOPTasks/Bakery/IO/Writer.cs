d8846b8cd421c763d4914651fbe0bbd3db3e3177
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2021-05-27 22:59:58 +0300

    OOPTasks

---
+++ C# OOP Exams/OOPExam121220/OOPTasks/Bakery/IO/Writer.cs
@@ -0,0 +1,43 @@
+﻿namespace Bakery.IO
+{
+    using Bakery.IO.Contracts;
+    using System;
+    using System.IO;
+
+    public class Writer : IWriter
+    {
+        //private string path = "../../../result.txt";
+        //public Writer()
+        //{
+        //    using (StreamWriter writer=new StreamWriter(path))
+        //    {
+        //        writer.Write("");
+        //    }
+        //}
+        //public void Write(string message)
+        //{
+        //    using (StreamWriter writer = new StreamWriter(path,true))
+        //    {
+        //        writer.Write(message);
+        //    }
+        //}
+
+        //public void WriteLine(string message)
+        //{
+        //    using(StreamWriter writer = new StreamWriter(path, true))
+        //    {
+        //        writer.WriteLine(message);
+        //    }
+        //}
+
+        public void Write(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
