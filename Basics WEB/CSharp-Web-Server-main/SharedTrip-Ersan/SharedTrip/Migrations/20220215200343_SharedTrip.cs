f6ef7a3c20128e5bca98b59136c7b0a904a67470
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-02-17 22:35:55 +0200

    Models

---
+++ Basics WEB/CSharp-Web-Server-main/SharedTrip-Ersan/SharedTrip/Migrations/20220215200343_SharedTrip.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SharedTrip.Migrations
+{
+    public partial class SharedTrip : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    StartPoint = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    EndPoint = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DepartureTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Seats = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(36)", maxLength: 36, nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "userTrips",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(36)", nullable: false),
+                    TripId = table.Column<string>(type: "nvarchar(36)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_userTrips", x => new { x.UserId, x.TripId });
+                    table.ForeignKey(
+                        name: "FK_userTrips_Trips_TripId",
+                        column: x => x.TripId,
+                        principalTable: "Trips",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_userTrips_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_userTrips_TripId",
+                table: "userTrips",
+                column: "TripId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "userTrips");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Basics WEB/CSharp-Web-Server-main/SharedTrip-Ersan/SharedTrip/Models/Users/LoginUserFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Models
+{
+    public class LoginUserFormModel
+    {
+
+        public string Username { get; init; }
+
+        public string Password { get; init; }
+    }
+}
