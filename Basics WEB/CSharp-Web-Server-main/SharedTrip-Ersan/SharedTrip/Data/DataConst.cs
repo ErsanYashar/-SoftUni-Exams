390120bd3d21d0dc0a1505cf39d7965d9bea1de6
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-02-17 22:35:40 +0200

    Data

---
+++ Basics WEB/CSharp-Web-Server-main/SharedTrip-Ersan/SharedTrip/Data/DataConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedTrip.Data
+{
+    public class DataConst
+    {
+        public const string UserEmailRegularExpression = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+    }
+}
