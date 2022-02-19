eed68eb7941b49c0910e467b9f63db5da5a3e953
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-02-19 17:43:17 +0200

    01._SMS_Skeleton-5.0

---
+++ Basics WEB/CSharp-Web-Server-main/01._SMS_Skeleton-5.0/01. SMS_Skeleton-5.0/SMS/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using SMS.Contracts;
+using System.Collections.Generic;
+
+namespace SMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IUserService userService;
+        private readonly IProductService productService;
+        //private readonly IProductService productService;
+
+        public HomeController(
+            Request request,
+            IUserService _userService, 
+            IProductService _productService)
+            : base(request)
+        {
+            userService = _userService;
+            productService = _productService;
+        }
+
+        public Response Index()
+        {
+      
+            if (User.IsAuthenticated)
+            {
+                string userName = userService.GetUsername(User.Id);
+                var model = new
+                {
+                    Username = userName,
+                    IsAuthenticated = true,
+                    Products = productService.GetProducts()
+                };
+
+                return View(model, "/Home/IndexLoggedIn");
+            }
+           
+            return this.View(new { IsAuthenticated=false});
+        }
+    }
+}
+
+
+
+
+
+
+
