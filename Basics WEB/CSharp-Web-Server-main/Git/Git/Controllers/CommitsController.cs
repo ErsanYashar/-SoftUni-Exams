2e7d0f38c7f12cf0eabd779343a81b0337777799
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-03-12 11:37:54 +0200

    CommitsController

---
+++ Basics WEB/CSharp-Web-Server-main/Git/Git/Controllers/CommitsController.cs
@@ -0,0 +1,75 @@
+﻿using Git.Data;
+using Git.Data.Models;
+using Git.Models.Commits;
+using Git.Services;
+using MyWebServer.Controllers;
+using MyWebServer.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Git.Data.DataConstants;
+
+namespace Git.Controllers
+{
+    public class CommitsController : Controller
+    {
+        private readonly ApplicationDbContext data;
+        public CommitsController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        [Authorize]
+        public HttpResponse Create(string id)
+        {
+            var repository = this.data
+                .Repositories
+                .Where(r => r.Id == id)
+                .Select(r => new
+                {
+                    Id = r.Id,
+                    Name = r.Name,
+                })
+                .FirstOrDefault();
+
+            if (repository == null)
+            {
+                return BadRequest();
+            }
+
+            return View(repository);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public HttpResponse Create(CreateCommitFormModel model)
+        {
+            if (!this.data.Repositories.Any(r => r.Id == model.Id))
+            {
+                return NotFound();
+            }
+
+            if (model.Description.Length < CommitMinDescription)
+            {
+                return Error($"Commit description have be at least {CommitMinDescription} characters.");
+            }
+
+            var commit = new Commit
+            {
+                Description = model.Description,
+                RepositoryId = model.Id,
+                CreatorId = this.User.Id
+            };
+
+            this.data.Commits.Add(commit);
+
+            this.data.SaveChanges();
+
+            return Redirect("/Repositories/All");
+        }
+
+    }
+    
+}
